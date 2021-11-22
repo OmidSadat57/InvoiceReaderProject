4d96fb33a20a42f2f74c68b7669fc458c6bef4c9
jmanc <jmancook@gmail.com>
2021-11-22 22:02:51 +0100

    - changes to PDF_SEPERATOR.py

--- PDF_SEPERATOR.py
+++ PDF_SEPERATOR.py
@@ -9,16 +9,19 @@ import pdf2image
 pytesseract.pytesseract.tesseract_cmd = r'C:/Users/jmanc/AppData/Local/Programs/Tesseract-OCR/tesseract.exe'
 
 print('Start conversion')
-pdf = pdf2image.convert_from_path('1. Vorlesung_RN_15.10.2019.pdf', 350)
+pdf = pdf2image.convert_from_path('pdfs/2020-12-22-muster-der-vordrucke-im-umsatzsteuer-voranmeldungs-und-vorauszahlungsverfahren-fuer-das-kalenderjahr-2021.pdf', 350)
 print('conversion finished')
 
 # img = 'test.PNG'
 
 # Simple image to string
 
-#print(pytesseract.image_to_string(pdf[0]))
+# print(pytesseract.image_to_string(pdf[0]))
 
-for number, page in enumerate(pdf, start=1):
+size = len(pdf)
+
+for number, page in enumerate(pdf, start=1015):
     page.save(f'image/page{number}.png')
+    print(f'Complete: {number} / {size + 1014}')  # 1025 ins Gesamt 22-11-2021
 
 # print(pytesseract.image_to_string(img))
