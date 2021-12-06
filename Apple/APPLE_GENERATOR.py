76b68b8a1fd823d97e50aae9eab28200a816476f
Jerome <s0574375@htw-berlin.de>
2021-12-06 12:47:30 +0100

    - added CSV for Apple - updated APPLE_GENERATOR.py

--- Apple/APPLE_GENERATOR.py
+++ Apple/APPLE_GENERATOR.py
@@ -1,44 +1,55 @@
 from __future__ import print_function
-
-from datetime import date
-
 from mailmerge import MailMerge
+import pandas
 # from datetime import date
 from docx2pdf import convert
 
 template = "invoice_apple.docx"
+df = pandas.read_csv('Apple/Adressen_Apple.csv', ';', dtype={'KundenNr': str})
+p_df = pandas.read_csv('Products/product_computers.csv', ';')
 
-document = MailMerge(template)
-print(document.get_merge_fields())
+# print(document.get_merge_fields())
 
-document.merge(
-    Nme='DARTH',  # format = Jerome
-    LstNme='VADER',  # format = Cook
-    Street='Throne',
-    HsNr='1',
-    City='Deathstar',
-    PCode='000066',
-    Country='Galactic Empire',  # format = upper()
-    DeliveryDate='04.05.2021',  # format = dd.mm.yyyy
-    DueDate='04.05.2021',  # format = dd.mm.yyyy
-    InvNum='DDDDDDDDDI',  # len = 10, char = numbers + letters, , format = LLxxxxxxxx
-    ApOrNr='1234567890',  # len = 10, char = numbers
-    OrdDate='03.05.2021',  # format = dd.mm.yyyy
-    CNr='123456',  # len = 6, char = numbers
-    OrdNum='A123456789',  # len = 10, char = numbers + letter , format = Lxxxxxxxxx
-    DelivNum='1234567890',  # len = 10, char = numbers
-    ProdNum='123456',  # len = 6, char = numbers
-    MatNum='ABC12DE/F',  # len = 9, char = numbers + letters, format = LLLxxLL/L
-    Product='Super Laser - COLOR : Green, full power',  # items from Apple product list
-    Prc='999,00',  # max len = xxx,xx
-    n='10',  # max len = xx
-    x='4,99',  # max len = xx,xx
-    p='19',  # max len = xx
-    y='5,99',  # max len = xx,xx
-    z='99,00',  # max len = xx,xx
-    s0='599,99',  # max len = xxx,xx
-    s1='9,99')  # max len = x,xx
+for index, row in df.iterrows():
 
-document.write('invoice-output.docx')
+    if index >= 2:
+        break
 
-convert('../invoice-output.docx', f'C:/Users/jmanc/PycharmProjects/InvoiceReaderProject/output.pdf')
+    try:
+
+        document = MailMerge(template)
+
+        document.merge(
+            Nme=row['Vorname'],  # format = Jerome
+            LstNme=row['Name'],  # format = Cook
+            Street=row['Straße'],
+            HsNr=str(row['Hausnummer']),
+            City=row['Ort'],
+            PCode=str(row['PLZ']),
+            Country='Germany',  # format = upper()
+            DeliveryDate=row['Bestelldatum'],  # format = dd.mm.yyyy
+            DueDate=row['Lieferdatum'],  # format = dd.mm.yyyy
+            InvNum='invoice_number',  # len = 10, char = numbers + letters, , format = LLxxxxxxxx
+            ApOrNr='0123456789',  # len = 10, char = numbers
+            OrdDate=row['Bestelldatum'],  # format = dd.mm.yyyy
+            CNr=str(row['KundenNr']),  # len = 6, char = numbers
+            OrdNum='ABCD123456',  # len = 10, char = numbers + letter , format = Lxxxxxxxxx
+            DelivNum='0123456789',  # len = 10, char = numbers
+            ProdNum='123456',  # len = 6, char = numbers
+            MatNum='LLLXXLL/L',  # len = 9, char = numbers + letters, format = LLLxxLL/L
+            Product=p_df.iloc[index, 0][:33],  # items from Apple product list, stings sliced at len 33
+            Prc=str(row['Betrag']),  # max len = xxx,xx
+            n=str(row['n']),  # max len = xx
+            x=str(row['Stückpreis ohne Ust']),  # max len = xx,xx
+            p=str(row['Ust. % Satz']),  # max len = xx
+            y=str(row['Stückpreis mit Ust']),  # max len = xx,xx
+            z=str(row['ZS Produkt']),  # max len = xx,xx
+            s0=str(row['Zs ohne Ust']),  # max len = xxx,xx
+            s1=str(row['Gesamt Ust. Betrag']))  # max len = x,xx
+
+        document.write(f'testing/docx/amazon-output{index + 1}.docx')
+
+        convert(f'testing/docx/amazon-output{index + 1}.docx', f'C:/Users/jmanc/PycharmProjects/InvoiceReaderProject/testing/amazon{index + 1}.pdf')
+
+    except:
+        pass
