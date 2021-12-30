642aa6ca068f3b6f2908d3c9d02b6adb287b1455
DuncanEb <duncanebner@icloud.com>
2021-12-30 11:45:17 +0100

    invoice data, generator, template

--- HP/HP_GENERATOR.py
+++ HP/HP_GENERATOR.py
@@ -2,19 +2,28 @@ from __future__ import print_function
 from mailmerge import MailMerge
 from docx2pdf import convert
 import pandas as pd
+from datetime import datetime
+from decimal import Decimal
 
-data = pd.read_csv('C:/Studium/6. Semester/Unternehmenssoftware/Projektarbeit/Invoice_HP_data(csv).csv')
 
-for index, row in data.iterrows():
+excel_data = pd.read_excel('C:/Studium/6. Semester/Unternehmenssoftware/repo/InvoiceReaderProject/HP/Invoice_HP_data.xlsx')
 
-    if index == 3:  # use index == 200 --> if you want to generate 200 invoices
+excel_data.to_csv('C:/Studium/6. Semester/Unternehmenssoftware/repo/InvoiceReaderProject/HP/Invoice_HP_data_csv.csv', index=False)
+
+for index, row in excel_data.iterrows():
+
+    if index == 200:  # use index == 200 --> if you want to generate 200 invoices
         break
 
-    template = "C:/Studium/6. Semester/Unternehmenssoftware/Projektarbeit/Invoice_HP_data(csv)invoice_hp.docx"
+    template = "C:/Studium/6. Semester/Unternehmenssoftware/repo/InvoiceReaderProject/HP/invoice_hp.docx"
     document = MailMerge(template)
+    
+    orderdate = datetime.date(row['CustomerOrderDate'])
+    deliverydate = datetime.date(row['DeliveryDate'])
+
     document.merge(
-        Name=row['Name'].upper(),  # format = upper()
-        Lastname=row['Lastname'].upper(),  # format = upper()
+        Name=row['Name'],  # format = upper()
+        Lastname=row['Lastname'],  # format = upper()
         Street=row['Street'],
         HouseNumber=str(row['HouseNumber']),
         PostalCode=str(row['PostalCode']),
@@ -22,22 +31,22 @@ for index, row in data.iterrows():
         Country=row['Country'],
         HPOrderNumber=row['HPOrderNumber'],
         CustomerOrderNumber=row['CustomerOrderNumber'],
-        CustomerOrderDate=row['CustomerOrderDate'],
+        CustomerOrderDate=str(orderdate),
         CustomerNumber=str(row['CustomerNumber']),
         HPUSTID=row['HPUSTID'],
         n=str(row['n']),
         PNr=row['PNr'],
         Product=row['Produkt'],
         LSNr=str(row['LSNr']),
-        DeliveryDate=row['DeliveryDate'],  # format = DD Month YYYY
+        DeliveryDate=str(deliverydate),  
         PackID=row['PackID'],
         SerialNr=['SerialNr'],
-        x=str(row['x']),
-        s00=str(row['s00']),
-        a=str(row['a']),
-        s0=str(row['s0']),
-        s1=str(row['s1']),
-        total=str(row['total']),
+        x=str(round(Decimal(row['x']), 2)).replace('.',','),
+        s00=str(round(Decimal(row['s00']), 2)).replace('.',','),
+        a=str(round(Decimal(row['a']), 2)).replace('.',','),
+        s0=str(round(Decimal(row['s0']), 2)).replace('.',','),
+        s1=str(round(Decimal(row['s1']), 2)).replace('.',','),
+        total=str(round(Decimal(row['total']), 2)).replace('.',','),
         InvoiceNumber=str(row['InvoiceNumber'])
     )
 
--- Kaspersky/KASPERSKY_GENERATOR.py
+++ Kaspersky/KASPERSKY_GENERATOR.py
@@ -2,16 +2,24 @@ from __future__ import print_function
 from mailmerge import MailMerge
 from docx2pdf import convert
 import pandas as pd
+from decimal import Decimal
+from datetime import datetime
 
-data = pd.read_csv('C:/Studium/6. Semester/Unternehmenssoftware/Projektarbeit/Invoice_Kaspersky_data(csv).csv')
+excel_data = pd.read_excel('C:/Studium/6. Semester/Unternehmenssoftware/repo/InvoiceReaderProject/Kaspersky/Invoice_Kaspersky_data.xlsx')
 
-for index, row in data.iterrows():
+excel_data.to_csv('C:/Studium/6. Semester/Unternehmenssoftware/repo/InvoiceReaderProject/Kaspersky/Invoice_Kaspersky_data_csv.csv', index=False)
+
+
+for index, row in excel_data.iterrows():
 
     if index == 3:  # use index == 200 --> if you want to generate 200 invoices
         break
 
-    template = "C:/Studium/6. Semester/Unternehmenssoftware/Projektarbeit/invoice_kaspersky.docx"
+    template = "C:/Studium/6. Semester/Unternehmenssoftware/repo/InvoiceReaderProject/Kaspersky/invoice_kaspersky.docx"
     document = MailMerge(template)
+    
+    d = datetime.date(row['InvoiceDate'])
+    
     document.merge(
         Name=row['Name'].upper(),  # format = upper()
         Lastname=row['Lastname'].upper(),  # format = upper()
@@ -22,16 +30,16 @@ for index, row in data.iterrows():
         Country=row['Country'],
         email=row['email'],
         InvoiceID=row['InvoiceID'],
-        InvoiceDate=row['InvoiceDate'],
+        InvoiceDate=str(d),
         OrderNr=row['OrderNr'],
         Ref=row['Ref'],
         Product=row['Product'],
         n=str(row['n']),
-        s00=str(row['s00']),
-        x=str(row['x']),
-        s1=str(row['s1']),
-        total=str(row['total']),
+        s00=str(round(Decimal(row['s00']), 2)).replace('.',','),
+        x=str(round(Decimal(row['x']), 2)).replace('.',','),
+        s1=str(round(Decimal(row['s1']), 2)).replace('.',','),
+        total=str(round(Decimal(row['total']), 2)).replace('.',','),
     )
 
     document.write('kaspersky-invoice-output.docx')
-    convert(f'kaspersky-invoice-output.docx', f'C:/Studium/6. Semester/Unternehmenssoftware/Projektarbeit/invoices_kaspersky/kaspersky-invoice-output{index}.pdf')
+    convert(f'kaspersky-invoice-output.docx', f'C:/Studium/6. Semester/Unternehmenssoftware/Projektarbeit/invoices_kas/kaspersky-invoice-output{index}.pdf')
