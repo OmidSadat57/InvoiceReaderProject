87fd4db94091a973d7560e19ea6d3bbe4bde3dc3
Jerome <s0574375@htw-berlin.de>
2022-01-13 21:37:02 +0100

    - changes OCR_MODEL.py

--- CLASSIFICATION_MODEL/OCR_MODEL.py
+++ CLASSIFICATION_MODEL/OCR_MODEL.py
@@ -3,6 +3,7 @@ import cv2
 import glob
 import numpy as np
 import tqdm
+import pandas
 
 # Tutorial: https://www.youtube.com/watch?v=9FCw1xo_s0I&list=PL2VXyKi-KpYuTAZz__9KVl1jQz74bDG7i&index=7
 
@@ -11,13 +12,11 @@ from Mod_Scripts.img_mods.IMG_PROCESSING import images
 
 pytesseract.pytesseract.tesseract_cmd = r'C:/Users/jmanc/AppData/Local/Programs/Tesseract-OCR/tesseract.exe'
 
-image_list = []
-
-# img_size = 250
-
 # images = glob.glob('C:/Users/jmanc/Documents/Jerome Dokus/HTW Studium/Semester 5_HTW/Unternehmenssoftware/Invoice_DATA/Invoices_Amazon/images/*.png')
-images = glob.glob('C:/Users/jmanc/Documents/Jerome Dokus/HTW Studium/Semester 5_HTW/Unternehmenssoftware/Invoice_DATA/Invoices_Amazon/images/*.png')
-# images = glob.glob('C:/Users/jmanc/PycharmProjects/InvoiceReaderProject/Apple/Data/img/*.png')
+# images = glob.glob('C:/Users/jmanc/Documents/Jerome Dokus/HTW Studium/Semester 5_HTW/Unternehmenssoftware/Invoice_DATA/Invoices_Pro-clipper/images/*.png')
+images = glob.glob('C:/Users/jmanc/PycharmProjects/InvoiceReaderProject/Apple/Data/img/*.png')
+
+df = pandas.DataFrame(columns=['Person', 'Street', 'City_PostalCode', 'Country'])
 
 # save images in list
 for rechnum, img in enumerate(tqdm.tqdm(images)):
@@ -33,23 +32,48 @@ for rechnum, img in enumerate(tqdm.tqdm(images)):
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
     cnts = sorted(cnts, key=lambda x: cv2.boundingRect(x)[0])
 
-    for num, c in enumerate(cnts):
-        x, y, w, h, = cv2.boundingRect(c)
-        # if h > 200 and w > 200:
-        roi = imgx[y:y+h, x:x+w]
-        cv2.imwrite(f'Boxes/{rechnum}/box{num}.png', roi)
-        cv2.rectangle(imgx, (x, y), (x+w, y+h), (36, 255, 12), 2)
+    box = cnts[7]
+    x, y, w, h, = cv2.boundingRect(box)
+    roi = imgx[y:y + h, x:x + w]
 
-    cv2.imwrite(f'Output/testBoxOutput{rechnum}.png', imgx)
+    ocr_string = pytesseract.image_to_string(roi)
+    ocr_string = ocr_string.strip('\x0c')
+    # ocr_string = ocr_string.replace('\n', ' ')
+    ocr_string_list = ocr_string.split('\n')
+
+    # filter out blanks
+    for item in ocr_string_list:
+        if item == '':
+            ocr_string_list.remove(item)
+
+    print(ocr_string_list)
+
+    # if len(ocr_string_list) > 4:
+    #     for x in ocr_string_list:
+    #         print(type(x))
+
+    # print(ocr_string_list)
+
+    # Amazon
+    data = {
+        'Person': ocr_string_list[0],
+        'Street': ocr_string_list[1],
+        'City_PostalCode': ocr_string_list[2],
+        'Country': ocr_string_list[3]
+    }
+
+    # print(data)
+
+    df = df.append(data, ignore_index=True)
 
     if rechnum == 10:
         break
 
+    # !!! Box count start = 0 !!!
     # Amazon: Person Info = Box 7, Logo/Organization = Box 1 {iterations = 10}
     # Apple: Person Info = Box 7, Logo = Box 1 {iterations = 9}
+    # Pro-clipper: Person Info = Box 2, Logo = Box b {iterations = 13}
 
+# print(df)
 
-    # imgx = pytesseract.image_to_data(imgx[0])
-    # print(imgx)
-
-    # image_list.append(imgx)
+df.to_csv('outputData.csv', sep=';', encoding='utf-8')
