ba27e55ac7d65a61683dc564a7cba309bda17598
Jerome <s0574375@htw-berlin.de>
2022-01-15 23:30:59 +0100

    - added changes to Kaspersky_OCR_MODEL.py

--- CLASSIFICATION_MODEL/OCR_MODEL.py
+++ CLASSIFICATION_MODEL/OCR_MODEL.py
@@ -9,7 +9,7 @@ import tqdm
 # If you don't have tesseract executable in your PATH, include the following:
 pytesseract.pytesseract.tesseract_cmd = r'C:/Users/jmanc/AppData/Local/Programs/Tesseract-OCR/tesseract.exe'
 
-images = glob.glob('C:/Users/jmanc/Documents/Jerome Dokus/HTW Studium/Semester 5_HTW/Unternehmenssoftware/Invoice_DATA/Invoices_HP/images/*.png')
+images = glob.glob('C:/Users/jmanc/Documents/Jerome Dokus/HTW Studium/Semester 5_HTW/Unternehmenssoftware/Invoice_DATA/Invoices_Kaspersky/images/*.png')
 
 for rechnum, img in enumerate(tqdm.tqdm(images)):
 
@@ -18,9 +18,9 @@ for rechnum, img in enumerate(tqdm.tqdm(images)):
     gray = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
     blur = cv2.GaussianBlur(gray, (7, 7), 0)
     thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-    kernal = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 13))
+    kernal = cv2.getStructuringElement(cv2.MORPH_RECT, (12, 12))
     # changing iterations helps with identifying stuctures (compare 1 and 10)
-    dilate = cv2.dilate(thresh, kernal, iterations=5)
+    dilate = cv2.dilate(thresh, kernal, iterations=9)
     cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
     cnts = sorted(cnts, key=lambda x: cv2.boundingRect(x)[0])
@@ -29,11 +29,11 @@ for rechnum, img in enumerate(tqdm.tqdm(images)):
     for boxnum, c in enumerate(cnts):
         x, y, w, h, = cv2.boundingRect(c)
         roi = imgx[y:y + h, x:x + w]
-        cv2.imwrite(f'C:/Users/jmanc/PycharmProjects/InvoiceReaderProject/CLASSIFICATION_MODEL/HP_Test/Boxes/{rechnum}/box{boxnum}.png', roi)
+        cv2.imwrite(f'C:/Users/jmanc/PycharmProjects/InvoiceReaderProject/CLASSIFICATION_MODEL/Kaspersky_Test/Boxes/{rechnum}/box{boxnum}.png', roi)
         # if h > 200 and w > 200:
         cv2.rectangle(imgx, (x, y), (x + w, y + h), (36, 255, 12), 2)
 
-    cv2.imwrite(f'C:/Users/jmanc/PycharmProjects/InvoiceReaderProject/CLASSIFICATION_MODEL/HP_Test/Output/testBoxOutput{rechnum}.png', imgx)
+    cv2.imwrite(f'C:/Users/jmanc/PycharmProjects/InvoiceReaderProject/CLASSIFICATION_MODEL/Kaspersky_Test/Output/testBoxOutput{rechnum}.png', imgx)
 
     if rechnum >= 10:
         break
--- Mod_Scripts/pdf_mods/PDF_TO_IMAGE.py
+++ Mod_Scripts/pdf_mods/PDF_TO_IMAGE.py
@@ -11,10 +11,10 @@ import glob
 pytesseract.pytesseract.tesseract_cmd = r'C:/Users/jmanc/AppData/Local/Programs/Tesseract-OCR/tesseract.exe'
 
 # C:\Users\jmanc\Documents\Jerome Dokus\HTW Studium\Semester 5_HTW\Unternehmenssoftware\Invoice_DATA\Invoices_Amazon
-pdfs = glob.glob('C:/Users/jmanc/Documents/Jerome Dokus/HTW Studium/Semester 5_HTW/Unternehmenssoftware/Invoice_DATA/Invoices_HP/*.pdf')
+pdfs = glob.glob('C:/Users/jmanc/Documents/Jerome Dokus/HTW Studium/Semester 5_HTW/Unternehmenssoftware/Invoice_DATA/Invoices_Kaspersky/*.pdf')
 
 # save images in list
 for x, pdf in enumerate(tqdm.tqdm(pdfs)):
     pdf_to_image = pdf2image.convert_from_path(pdf, 400)
-    pdf_to_image[0].save(f'C:/Users/jmanc/Documents/Jerome Dokus/HTW Studium/Semester 5_HTW/Unternehmenssoftware/Invoice_DATA/Invoices_HP/images/xing{x}.png')
+    pdf_to_image[0].save(f'C:/Users/jmanc/Documents/Jerome Dokus/HTW Studium/Semester 5_HTW/Unternehmenssoftware/Invoice_DATA/Invoices_Kaspersky/images/kaspersky{x}.png')
 
